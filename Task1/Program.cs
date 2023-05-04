8fc07feaa8f8916ac5340d70fe0135b7a7c292fd
Aslan <aslan.aslan.14@bk.ru>
2023-05-04 17:09:35 +0600

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿string? str = System.Console.ReadLine();
+
+if (str![0] == str![4] && str![1] == str![3] && str![2] != str![0] && str![2] != str![1])
+{
+    System.Console.WriteLine($"{str} - correct!");
+}
+else
+{
+    System.Console.WriteLine($"{str} - incorrect!");
+}
