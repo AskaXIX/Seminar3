f75de50b86432f86b38c2450ec188fc0203dc0da
Aslan <aslan.aslan.14@bk.ru>
2023-05-04 17:14:56 +0600

    add changes to Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,7 @@
-﻿string? str = System.Console.ReadLine();
+﻿System.Console.WriteLine("Введите 5 значное число: ");
+string? str = System.Console.ReadLine();
 
-if (str![0] == str![4] && str![1] == str![3] && str![2] != str![0] && str![2] != str![1])
+if (str!.Length == 5 && str![0] == str![4] && str![1] == str![3] && str![2] != str![0] && str![2] != str![1])
 {
     System.Console.WriteLine($"{str} - correct!");
 }
